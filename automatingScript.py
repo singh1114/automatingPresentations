956bf66deacb17fe23eb9393139464d0b23ff0f3
singh1114 <ranvir.singh1114@gmail.com>
2017-02-10 01:34:50 +0530

    basic program made
    run the python script and result.md file will be made
    other configuration settings for opening presentation will be shared in readme file
    some things that are needed to be take care are:
    
    Even if only the first word is in uppercase it is transforming it to h1
    Some problem in collecting the para

---
+++ automatingScript.py
@@ -0,0 +1,72 @@
+# Read the file:
+#
+#   whenever the line starts with *:
+#
+#     read each character:
+#
+#       if the letters are capital:
+#         give heading h1
+#
+#       if the * is first in the group:
+#         raise the height by 50%
+#
+#       if there is nothing after *
+#         do nothing
+#
+#   if the line starts with References:
+#     write the code for new slides
+
+# open the given file in read mode
+inputFile = open("FinInfSt.txt", "r")
+
+# open another file where the changes are going to be made
+outputFile = open("result.md", "w")
+
+# temporary variable to check for the new slides
+tempSlide = 1
+
+# iterating over each line in the file
+for line in inputFile:
+
+    # spliting all the words in  the file
+    words = line.split()
+    # print words
+    # print "\n"
+
+    # iterating over all the words in a particular line
+    for x in words:
+        if(x == "References"):
+            outputFile.write('\n---\n\n')
+            tempSlide = 1
+
+        if(x == "*"):
+            if(tempSlide == 1):
+                # Don't make a list
+                # Set height = 150%
+                tempSlide = 0
+                continue
+
+        if((x.isupper() and tempSlide == 0 and x != "*") or (x.isupper() and tempSlide == 2)):
+            somestring1 = "# "
+            newString = line
+            newString = newString.replace('* ', '')
+            outputFile.write(somestring1 + newString + '\n')
+            tempSlide = 2
+            break
+
+        elif(tempSlide == 0 and x != "*"):
+            somestring1 = "### "
+            newString = line
+            newString = newString.replace('* ', '')
+            outputFile.write(somestring1 + newString + '\n')
+            tempSlide = 2
+            break
+
+        elif(tempSlide == 2 and x != "*"):
+            somestring1 = "* "
+            newString = line
+            newString = newString.replace('* ', '')
+            outputFile.write(somestring1 + newString + '\n')
+            break
+
+        #elif(tempSlide == 2):
